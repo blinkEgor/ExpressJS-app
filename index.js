accf1711d7d1229f6131202788e72df926d53f8d
blinkEgor <barminov44@gmail.com>
2023-09-01 14:01:18 +0300

    res

--- index.js
+++ index.js
@@ -4,14 +4,28 @@ const port = 8404;
 
 app.get("/", (req, res) => {
     console.log(`New request made: ${req.method}`); // GET
-    console.log(`Hostname: ${req.hostname}`); // localhost
-    if (req.hostname === 'localhost') {
-        console.log('Request Accepted'); // true
-    } else {
-        console.log('Request Denied'); // false
-    }
-    console.log(`Country parameter: ${req.params.country}`); // undefined
-    console.log(`Thet pet's name is: ${req.params.petName}`); // undefined
+    res.send("<h1>This is the response</h1>"); // This is the response
+});
+/*
+app.get("/about", (req, res) => {
+    console.log(`New request made: ${req.method}`); // GET
+    res.status(200).send("<p>Successful Request</p>"); // Successful Request
+});
+*/
+app.get("/about", (req, res) => {
+    console.log(`New request made: ${req.method}`); // GET
+    res.send("<h1>About</h1>"); // About
+});
+
+app.get("/user", (req, res) => {
+    console.log(`New request made: ${req.method}`); // GET
+    res.redirect("/userinfo");
+    res.send("<h1>User Info Page</h1>"); // User Info Page
+});
+
+app.get("/userinfo", (req, res) => {
+    console.log(`New request made: ${req.method}`); // GET
+    res.send("<h1>User Info Page</h1>"); // User Info Page
 });
 
 app.listen(port, () => {
