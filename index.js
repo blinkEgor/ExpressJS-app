48c106946688965e30a3067e1284e3900e314c39
blinkEgor <barminov44@gmail.com>
2023-09-01 12:54:16 +0300

    res

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const app = express();
-const port = 8000;
+const port = 8404;
+
+app.get("/", (req, res) => {
+    console.log(`New request made: ${req.method}`); // GET
+    console.log(`Hostname: ${req.hostname}`); // localhost
+    if (req.hostname === 'localhost') {
+        console.log('Request Accepted'); // true
+    } else {
+        console.log('Request Denied'); // false
+    }
+    console.log(`Country parameter: ${req.params.country}`); // undefined
+    console.log(`Thet pet's name is: ${req.params.petName}`); // undefined
+});
 
 app.listen(port, () => {
-    console.log(`The server is listening on port ${port}`);
+    console.log(`The server is listening on port ${port}`); // 8404
 });
