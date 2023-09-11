95046d736c9f179fb81632e5075189545e42852e
blinkEgor <barminov44@gmail.com>
2023-09-11 11:10:16 +0300

    Connecting to a Database

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
+/* эмитация базы данных */ 
 const products = [
     { id: 1, category: 'electronics', price: 400, title: 'phone' },
     { id: 2, category: 'electronics', price: 900, title: 'tv' },
     { id: 3, category: 'electronics', price: 200, title: 'vacuum' },
 ];
+/* -------------------- */
 
+/* подключение к базе данных */ 
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'myUser',
+    password: 'myPassword',
+    database: 'myDB'
+});
+/* ------------------------- */
+
+/* подключение express */
 const express = require('express');
 const app = express();
 const port = 8404;
+/* ------------------- */
 
-app.post('/products/:id', (req, res) => {
-    const newProduct = req.body;
-    products.push(newProduct);
-    const confirmedEntry = products.find(product => 
-        product === newProduct);
-    if(confirmedEntry) {
-        res.status(201).send(confirmedEntry);
-    } else {
-        res.status(400).send("Error Creating Product");
-    }
-});
-
+/* финальный прослушиватель событий */ 
 app.listen(port, () => {
     console.log(`The server is listening on port ${port}`);
 });
+/* -------------------------------- */
