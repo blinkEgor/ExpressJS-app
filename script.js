6afe559d39438c3a0b7f2934d761f8f5280d4ad1
blinkEgor <barminov44@gmail.com>
2023-09-13 15:02:06 +0300

    Database Interactions

--- script.js
+++ script.js
@@ -6,30 +6,6 @@ const products = [
 ];
 /* -------------------- */
 
-/* подключение к базе данных */ 
-require('dotenv').config();
-const mysql = require('mysql');
-const con = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-});
-    // инструкция подключения
-con.connect(err => {
-    if(err) { throw err }
-    con.query("SELECT * FROM Customers", (error, result) => {
-        if(error) throw error;
-        console.log(result[2].name);
-    });
-});
-    // прерывание соединения (рекомендуется)
-con.end(error => {
-    if(error) { throw error }
-    console.log('Connection closed!');
-});
-/* ------------------------- */
-
 /* подключение express */
 const express = require('express');
 const app = express();
@@ -41,3 +17,33 @@ app.listen(port, () => {
     console.log(`The server is listening on port ${port}`);
 });
 /* -------------------------------- */
+
+/* подключение к базе данных */ 
+require('dotenv').config();
+const mysql = require('mysql');
+const con = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+});
+    // инструкция подключения
+con.connect(err => {
+    if(err) throw err;
+    con.query("INSERT INTO customers (name, yearFounded) VALUES ?",
+    [
+        ['ABC Company', '2001'],
+        ['DEF Corp', '1998'],
+        ['GHI Inc.', '2015']
+    ], 
+    (error, result) => {
+        if(error) throw error;
+        console.log(result.affectedRows);
+    });
+});
+    // прерывание соединения (рекомендуется)
+con.end(error => {
+    if(error) { throw error }
+    console.log('Connection closed!');
+});
+/* ------------------------- */
