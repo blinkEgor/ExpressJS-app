7a8a0e53c8975f08ddefbe7f29f19d45cf37100f
blinkEgor <barminov44@gmail.com>
2023-10-05 13:28:38 +0300

    Create routes and listening on a port

--- index.js
+++ index.js
@@ -3,3 +3,13 @@
 
 const express = require('express');
 const app = express();
+
+// Create routes
+app.get('/', (req, res) => {
+    res.send({message: 'Hello WWW!'});
+});
+
+// listening on a port
+app.listen(3333, () => {
+    console.log('Application listening on port 3333!');
+});
